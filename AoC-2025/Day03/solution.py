fc06af7e6892258c6e0e725564058261b6ad805f
Yunus Ahmet Sari <yunus-sari61@hotmail.de>
2025-12-06 20:20:14 +0100

    Add Advent of Code 2025 Day 3 input data and Python solution script.

---
+++ AoC-2025/Day03/solution.py
@@ -0,0 +1,23 @@
+# Day 3 https://adventofcode.com/2025/day/3
+
+with open("input.txt") as f:
+    ls = f.read().strip().split("\n")
+
+
+def calculate_sum(data, num_batteries):
+    total_sum = 0
+    for i in data:
+        list_batteries = []
+        index = 0
+        for j in reversed(range(0, num_batteries)):
+            slice_end = len(i) - j
+            highest_value = max(i[index:slice_end])
+            highest_value_index = i.index(highest_value, index)
+            index = highest_value_index + 1
+            list_batteries.append(highest_value)
+        str_batteries = "".join(list_batteries)
+        total_sum += int(str_batteries)
+    return total_sum
+
+print(calculate_sum(ls, 2))
+print(calculate_sum(ls, 12))
